18124ebe88d70cadfe84b67a735b1053d5cb0df6
Thomas Stone <146038044+Gy6Security@users.noreply.github.com>
2024-05-12 14:42:28 -0400

    player
    Player movement,sprint,jump and camera movement completed

--- UnityProjectDD/Project Shadow/Assets/scripts/CameraController.cs
+++ UnityProjectDD/Project Shadow/Assets/scripts/CameraController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
+
+    [SerializeField] int sens;
+
+    [SerializeField] int lockVertMin, lockVertMax;
+
+    [SerializeField] bool invertY;
+
+    float rotX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+
+        Cursor.lockState = CursorLockMode.Locked; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sens;
+
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sens;
+
+        if (invertY)
+        {
+            rotX += mouseY;
+        }
+        else
+        {
+            rotX -= mouseY;
+        }
+
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        transform.parent.Rotate(Vector3.up * mouseX);
         
+
     }
+
+    
 }
--- UnityProjectDD/Project Shadow/Assets/scripts/PlayerController.cs
+++ UnityProjectDD/Project Shadow/Assets/scripts/PlayerController.cs
@@ -2,17 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IDamage
 {
+
+    [SerializeField] CharacterController playerControls;
+
+    [SerializeField] int HP;
+
+    [SerializeField] int speed;
+
+    [SerializeField] int sprintMod;
+
+    [SerializeField] int jumpMax;
+
+    [SerializeField] int jumpSpeed;
+
+    [SerializeField] int grav;
+
+    [SerializeField] int shootDamage;
+
+    [SerializeField] int shootRate;
+
+    [SerializeField] int shootDist;
+
+    Vector3 moveDir;
+
+    Vector3 playerVel;
+
+    int jumpCount;
+
+    int HPOrig;
+
+    bool isShooting;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HPOrig = HP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
     }
+
+    void movement()
+    {
+        if (playerControls.isGrounded)
+        {
+            jumpCount = 0;
+
+            playerVel = Vector3.zero;
+        }
+        
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) +
+                  (Input.GetAxis("Vertical") * transform.forward);
+
+        playerControls.Move(moveDir * speed * Time.deltaTime);
+
+        sprint();
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+
+            jumpCount++;
+
+            playerVel.y = jumpSpeed;
+
+        }
+
+        playerVel.y -= grav * Time.deltaTime;
+
+        playerControls.Move(playerVel * Time.deltaTime);
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+    }
+
 }
