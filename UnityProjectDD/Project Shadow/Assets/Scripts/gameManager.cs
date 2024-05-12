c375764c063d98a71fae4302a2aca4b8b89bcfe9
elij77 <eli.jeffery@live.com>
2024-05-12 13:06:20 -0700

    added fields to gameManager, added a field for player damage feedback panel to UI
    not a significant commit but wanted to make sure group had most current work

--- UnityProjectDD/Project Shadow/Assets/Scripts/gameManager.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/gameManager.cs
@@ -9,22 +9,52 @@ public class gameManager : MonoBehaviour
 {
     public static gameManager instance;
 
-    public GameObject player;
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] GameObject enemyCountText;
 
-    
+
+    public GameObject playerFlashDamage;
+    public Image playerHPBar;
+
+    public GameObject player;
+    public playerController playerScript;
+
+    public bool isPaused;
+    int enemyCount;
 
     // Start is called before the first frame update
     void Awake()
     {
         instance = this;
-
         player = GameObject.FindWithTag("Player");
-
-
+        playerScript = player.GetComponent<playerController>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void statePause()
+    {
+
+    }
+
+    public void stateUnPause()
+    {
+
+    }
+
+    public void updateGameGoal()
+    {
+
+    }
+
+    public void loseMenu()
     {
         
     }
--- UnityProjectDD/Project Shadow/Assets/scripts/PlayerController.cs
+++ UnityProjectDD/Project Shadow/Assets/scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IDamage
+public class playerController : MonoBehaviour, IDamage
 {
 
     [SerializeField] CharacterController playerControls;
