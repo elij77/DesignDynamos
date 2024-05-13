874afbda87b2906cd2994ebc6fdbe6af8a929617
PaladinLobo <143458028+PaladinLobo@users.noreply.github.com>
2024-05-12 17:56:53 -0700

    Bullet
    Bullet cod and projectile done

--- UnityProjectDD/Project Shadow/Assets/Scripts/bullet.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/bullet.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
+
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, destroyTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        IDamage dmg = other.gameObject.GetComponent<IDamage>();
+
+        if (dmg != null)
+        {
+            dmg.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
     }
+
 }
