5384011eebb4284fc67faf0a5328a4f905645aa6
Thomas Stone <146038044+Gy6Security@users.noreply.github.com>
2024-05-14 15:30:07 -0400

    fixes
    fixed AIHealth to track main camera instead of trying to add main camera to a field

--- UnityProjectDD/Project Shadow/Assets/scripts/AIHealth.cs
+++ UnityProjectDD/Project Shadow/Assets/scripts/AIHealth.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class AIHealth : MonoBehaviour
 {
     [SerializeField] private Slider slider;
-    [SerializeField] private Camera cam;
+    
     [SerializeField] private Transform position;
 
+    private Camera cam;
 
     public void updateHealthBar(float HP, float maxHP)
     {
         slider.value = HP / maxHP;
     }
-  
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
     void Update()
     {
         transform.rotation = cam.transform.rotation;
