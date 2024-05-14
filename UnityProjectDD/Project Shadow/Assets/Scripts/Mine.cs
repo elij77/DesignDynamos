c7e3b52be135799d6b2548d2256e8e958a06821a
Bradley Hamner <bradleyhamner85@gmail.com>
2024-05-14 15:59:20 -0500

    clean p

--- UnityProjectDD/Project Shadow/Assets/Scripts/Mine.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/Mine.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
-
     [SerializeField] int damage;
-    
-    int hp;
     
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("boom");
-        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
         {
-            IDamage dmg = other.gameObject.GetComponent<IDamage>();
-            // decreases the players health
+            IDamage dmg = other.transform.GetComponent<IDamage>();
 
-            dmg.takeDamage(damage);
+            if (dmg != null)
+            {
+                dmg.takeDamage(damage);
+            }
             Destroy(gameObject);
 
         }
