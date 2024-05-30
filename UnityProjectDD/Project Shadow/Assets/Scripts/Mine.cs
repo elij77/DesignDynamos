15b9e352e58874b6ca6d6dd63cc44ec582629966
ConnorProsise <connorprosise@gmail.com>
2024-05-30 00:52:01 -0800

    Updated mine to play a sound when it explodes and can be shot
    Made new prefab

--- UnityProjectDD/Project Shadow/Assets/Scripts/Mine.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/Mine.cs
@@ -2,22 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mine : MonoBehaviour
+public class Mine : MonoBehaviour, IDamage
 {
     [SerializeField] int damage;
     [SerializeField] AudioClip explosion;
+    [SerializeField] AudioSource aud;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("boom");
         {
             IDamage dmg = other.transform.GetComponent<IDamage>();
-              
-            if (dmg != null )
-            dmg.takeDamage(damage);
-            
-            Destroy(gameObject);
 
+            if (dmg != null)
+            {
+                dmg.takeDamage(damage);
+                StartCoroutine(PlaySoundWithDelay());
+                //Destroy(gameObject);
+            }
+
+        }
+    }
+    public void takeDamage(int damage)
+    {
+        GetComponent<Renderer>().enabled = false;
+        transform.parent.gameObject.GetComponent<Renderer>().enabled = false; 
+        Debug.Log("destroyed");
+        StartCoroutine(PlaySoundWithDelay());
+        
+
+    }
+
+    private IEnumerator PlaySoundWithDelay()
+    {
+        
+        aud.PlayOneShot(explosion, 1f);
+        yield return new WaitForSeconds(1);
+        if (transform.parent != null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
