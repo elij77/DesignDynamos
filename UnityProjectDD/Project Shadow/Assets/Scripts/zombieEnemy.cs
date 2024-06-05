b88700ea22dd9b5bc5915fe7d3b1cbbf53c99ea4
elij77 <eli.jeffery@live.com>
2024-06-04 21:42:24 -0700

    Update zombieEnemy.cs
    zombieEnemy script work

--- UnityProjectDD/Project Shadow/Assets/Scripts/zombieEnemy.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/zombieEnemy.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class zombieEnemy : MonoBehaviour
+
+public class zombieEnemy : MonoBehaviour, IDamage
 {
+    [SerializeField] Animator anim;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform headPos;
+
+    [SerializeField] int viewAngle;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int animSpeedTrans;
+    [SerializeField] int roamDist;
+    [SerializeField] int roamTimer;
+
+    AIHealth health;
+
+    [SerializeField] int HP;
+    [SerializeField] int maxHP;
+
+    bool playerInRange;
+    bool destChosen;
+
+    float angleToPlayer;
+    float stoppingDistOrig;
+
+    Vector3 playerDir;
+    Vector3 startingPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
+        stoppingDistOrig = agent.stoppingDistance;
+
+        health = GetComponentInChildren<AIHealth>();
+        health.updateHealthBar(HP, maxHP);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float animSpeed = agent.velocity.normalized.magnitude;
+        anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), animSpeed, Time.deltaTime * animSpeedTrans));
+
+        if (playerInRange && !canSeePlayer())
+        {
+            StartCoroutine(roam());
+        }
+        else if (!playerInRange)
+        {
+            StartCoroutine(roam());
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+
+    }
+
+    bool canSeePlayer()
+    {
+
+
+        return false;
+    }
+
+    IEnumerator roam()
+    {
+
+
+        yield return new WaitForSeconds(roamTimer);
     }
 }
