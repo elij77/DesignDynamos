611d1c3a3985351f1fa90af5b317ce5c7609fe99
Thomas Stone <146038044+Gy6Security@users.noreply.github.com>
2024-06-08 23:38:19 -0400

    Main Menu
    Edited Audio Manager script to save value of volume in main menu smooth audio transition between menus are good (working on level volumes next and smooth transition)

--- UnityProjectDD/Project Shadow/Assets/Scripts/Audio Scripts/AudioManager.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/Audio Scripts/AudioManager.cs
@@ -1,16 +1,44 @@
 
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
+    [SerializeField] private Slider volumeSlider;
 
+    public AudioMixer audioMixer;
     public AudioClip background;
 
     private void Start()
     {
+        if (PlayerPrefs.HasKey("volume"))
+        {
+            load();
+        }
+        else
+        {
+            SetVolume();
+        }
         musicSource.clip = background;
         musicSource.Play();
     }
+
+    public void SetVolume()
+    {
+        float volume = volumeSlider.value;
+        //Debug.Log(volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+
+    private void load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+
+        SetVolume();
+    }
+    
 }
