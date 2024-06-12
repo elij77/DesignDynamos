9094758b99eebd3f109396a0953f2cc95d9c792d
Thomas Stone <146038044+Gy6Security@users.noreply.github.com>
2024-06-11 23:42:53 -0400

    Main Menu
    Graphics settings setup, Audio tested to smoothly transition
    Button Functions script being used for buttons

--- UnityProjectDD/Project Shadow/Assets/scripts/Game Manager Scripts/buttonFunctions.cs
+++ UnityProjectDD/Project Shadow/Assets/scripts/Game Manager Scripts/buttonFunctions.cs
@@ -5,56 +5,85 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class buttonFunctions : MonoBehaviour
 {
 
-    //public GameObject[] menuItems;
+    public GameObject[] menuItems;
 
-    //private int selectedItemIndex = 0;
+    private int selectedItemIndex = 0;
 
-    //public void Update()
-    //{
+    Resolution[] resolutions;
 
-    //    if (Input.GetKeyDown(KeyCode.UpArrow))
-    //    {
-    //        selectedItemIndex--;
-
-    //        if (selectedItemIndex < 0)
-    //        {
-    //            selectedItemIndex = menuItems.Length - 1;
-    //        }
-    //    }
-
-    //    else if (Input.GetKeyDown(KeyCode.DownArrow))
-    //    {
-    //        selectedItemIndex++;
-
-    //        if (selectedItemIndex >= menuItems.Length)
-    //        {
-    //            selectedItemIndex = 0;
-    //        }
-    //    }
-
-    //    else if (Input.GetKeyDown(KeyCode.Return))
-    //    {
-    //        menuItems[selectedItemIndex].GetComponent<Button>().onClick.Invoke();
-    //    }
-
-    //    for (int i = 0; i < menuItems.Length; i++)
-    //    {
-    //        if ( i == selectedItemIndex)
-    //        {
-    //            menuItems[i].GetComponent<Button>().Select();
-    //        }
-    //        else
-    //        {
-    //            menuItems[i].GetComponent<Button>().OnDeselect(null);
-    //        }
-    //    }
-    //}
+    public TMP_Dropdown resDropDown;
 
     // Start is called before the first frame update
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        
+        resDropDown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string res = resolutions[i].width + " x " + resolutions[i].height;
+            resOptions.Add(res);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height  == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resDropDown.AddOptions(resOptions);
+        resDropDown.value = currentResIndex;
+        resDropDown.RefreshShownValue();
+    }
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedItemIndex--;
+
+            if (selectedItemIndex < 0)
+            {
+                selectedItemIndex = menuItems.Length - 1;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedItemIndex++;
+
+            if (selectedItemIndex >= menuItems.Length)
+            {
+                selectedItemIndex = 0;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            menuItems[selectedItemIndex].GetComponent<Button>().onClick.Invoke();
+        }
+
+        for (int i = 0; i < menuItems.Length; i++)
+        {
+            if (i == selectedItemIndex)
+            {
+                menuItems[i].GetComponent<Button>().Select();
+            }
+            else
+            {
+                menuItems[i].GetComponent<Button>().OnDeselect(null);
+            }
+        }
+    }
+
+   
     public void resume()
     {
         gameManager.instance.stateUnPause();
@@ -129,5 +158,21 @@ public class buttonFunctions : MonoBehaviour
     public void miniFlash()
     {
         gameManager.instance.MiniToggle();
+    }
+
+    public void setGraphics(int Quality)
+    {
+        QualitySettings.SetQualityLevel(Quality);
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution resolution = resolutions[index];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
