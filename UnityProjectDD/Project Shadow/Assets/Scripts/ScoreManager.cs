8acc9cb54e78406159df910e0398fcd54be456ba
PaladinLobo <143458028+PaladinLobo@users.noreply.github.com>
2024-06-15 04:57:17 -0700

    Updated Score board issue

--- UnityProjectDD/Project Shadow/Assets/Scripts/ScoreManager.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/ScoreManager.cs
@@ -1,33 +1,30 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//public class ScoreManager : MonoBehaviour
-//{
-//    public static ScoreManager instance;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
 
-//    public Text killsText;
-//    public Text waveNumberText;
+    public Text killsText;
+    public Text waveNumberText;
 
-//    int waveNumber = 0;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
 
 
-//    private void Awake()
-//    {
-//        instance = this;
-//    }
+    }
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-      
-  
-//    }
+    // Update is called once per frame
+    void Update()
+    {
 
-//    // Update is called once per frame
-//    void Update()
-//    {
+    }
 
-//    }
-
-//}
+}
