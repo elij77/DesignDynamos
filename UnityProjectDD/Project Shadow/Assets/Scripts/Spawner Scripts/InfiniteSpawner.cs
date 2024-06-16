26ff71eee80d52687dcfe1ce72e5f605d673da5c
Thomas Stone <146038044+Gy6Security@users.noreply.github.com>
2024-06-16 17:07:21 -0400

    final push updated
    commented out infinite spawner

--- UnityProjectDD/Project Shadow/Assets/Scripts/Spawner Scripts/InfiniteSpawner.cs
+++ UnityProjectDD/Project Shadow/Assets/Scripts/Spawner Scripts/InfiniteSpawner.cs
@@ -1,51 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class InfiniteSpawner : MonoBehaviour
-{
-    [SerializeField] GameObject objectToSpawn;
-    [SerializeField] int spawnTimer;
-    [SerializeField] Transform spawnPos;
+//public class InfiniteSpawner : MonoBehaviour
+//{
+//    [SerializeField] GameObject objectToSpawn;
+//    [SerializeField] int spawnTimer;
+//    [SerializeField] Transform spawnPos;
 
-    int spawnCount;
-    bool isSpawning;
-    bool startSpawning;
+//    int spawnCount;
+//    bool isSpawning;
+//    bool startSpawning;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if (startSpawning && !isSpawning  && gameManager.instance.InfiniteSpawnerOn() )
-        //{
-        //    StartCoroutine(spawn());
-        //}
-        StartCoroutine(spawn());
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //if (startSpawning && !isSpawning  && gameManager.instance.InfiniteSpawnerOn() )
+//        //{
+//        //    StartCoroutine(spawn());
+//        //}
+//        StartCoroutine(spawn());
+//    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            startSpawning = true;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            startSpawning = false;
-        }
-    }
-    IEnumerator spawn()
-    {
-        isSpawning = true;
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            startSpawning = true;
+//        }
+//    }
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            startSpawning = false;
+//        }
+//    }
+//    IEnumerator spawn()
+//    {
+//        isSpawning = true;
         
-        Instantiate(objectToSpawn, spawnPos.position, spawnPos.rotation);
-        gameManager.instance.updateEnemyGoal(1);
-        yield return new WaitForSeconds(spawnTimer);
-        isSpawning = false;
+//        Instantiate(objectToSpawn, spawnPos.position, spawnPos.rotation);
+//        gameManager.instance.updateEnemyGoal(1);
+//        yield return new WaitForSeconds(spawnTimer);
+//        isSpawning = false;
 
 
-    }
-}
+//    }
+//}
